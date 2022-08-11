8ef343fe797dc99e86badc89df2ca9b07461f886
fullstack300622 <108460728+fullstack300622@users.noreply.github.com>
2022-08-11 21:26:08 +0300

    Create main.java

---
+++ main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        //final
+//        final int PLAYER_ID = 5;
+//        Person p1 = new Person(20,"Red");
+//        Person p2 = new Person(18,"black");
+//        System.out.println(p1.className);
+//        p2.className = "Pinhas";
+//        System.out.println(p2.className);
+//        System.out.println(p1.className);
+//        Person.returnClassName();
+//        System.out.println(p1.returnClassName());
+//        Person.getAge();
+
+//        Person.className="";
+    }
+}
